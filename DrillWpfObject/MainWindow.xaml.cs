899485e30d023364f6dc8367422eab5f210288ce
Karlsson0214 <karlsson0214@gmail.com>
2022-01-25 09:46:09 +0100

    Add project files.

---
+++ DrillWpfObject/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrillWpfObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Result> results = new List<Result>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            int distance = Convert.ToInt32(Distance.Text);
+            double time = Convert.ToDouble(Time.Text);
+
+            Result result = new Result(distance, NameOfRunner.Text, time);
+
+            Distance.Text = "";
+            NameOfRunner.Text = "";
+            Time.Text = "";
+
+            results.Add(result);
+
+            listOfResults.Text = "";
+
+            foreach(Result oneResult in results)
+            {
+                listOfResults.Text += oneResult.Name + Environment.NewLine;
+                listOfResults.Text += "Distance: " + oneResult.Distance 
+                    + " m" + Environment.NewLine;
+                listOfResults.Text += "Time: " + oneResult.Time + " s"
+                    + Environment.NewLine + Environment.NewLine;
+            }
+
+            
+        }
+    }
+}
---
+++ DrillWpfObject/Result.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrillWpfObject
+{
+    class Result
+    {
+        // objektvariabler
+        private int distance;
+        private string name;
+        private double time;
+
+        // konstruktor
+        public Result(int distance, string name, double time)
+        {
+            this.distance = distance;
+            this.name = name;
+            this.time = time;
+        }
+
+        // properties - egenskaper
+        public int Distance
+        {
+            get
+            {
+                return distance;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public double Time
+        {
+            get
+            {
+                return time;
+            }
+        }
+    }
+}
